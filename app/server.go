946a9cf092313da09e36bcd0aeb616067862404d
Szymon Bednarski <bednarski.szymon99@gmail.com>
2024-04-21 14:20:26 +0200

    handle echo request

--- app/server.go
+++ app/server.go
@@ -5,31 +5,59 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const (
-	CRLF      = "\r\n"
-	OK        = "HTTP/1.1 200 OK" + CRLF + CRLF
-	NOT_FOUND = "HTTP/1.1 404 Not Found" + CRLF + CRLF
+	CRLF                  = "\r\n"
+	OK                    = "HTTP/1.1 200 OK"
+	NOT_FOUND             = "HTTP/1.1 404 Not Found" + CRLF + CRLF
+	BAD_REQUEST           = "HTTP/1.1 400 Bad Request" + CRLF + CRLF
+	INTERNAL_SERVER_ERROR = "HTTP/1.1 500 Internal Server Error" + CRLF + CRLF
+	CONTENT_TYPE          = "Content-Type: "
+	CONTENT_LENGTH        = "Content-Length: "
 )
 
 const (
+	TEXT_PLAIN = "text/plain"
+)
+
+const (
+	ECHO          = "/echo/"
 	FORWARD_SLASH = "/"
 	WHITESPACE    = " "
 )
 
-type HttpHeader struct {
+type RequestHeader struct {
 	Method  string
 	Path    string
 	Version string
 }
 
-func CreateHeader(method, path, version string) *HttpHeader {
-	return &HttpHeader{
+func CreateHeader(method, path, version string) *RequestHeader {
+	return &RequestHeader{
 		Method:  method,
 		Path:    path,
 		Version: version,
 	}
+}
+
+func HandleEchoRequest(conn net.Conn, header *RequestHeader) {
+	data, found := strings.CutPrefix(header.Path, ECHO)
+
+	if !found {
+		conn.Write([]byte(INTERNAL_SERVER_ERROR))
+	}
+	contentType := CONTENT_TYPE + TEXT_PLAIN + CRLF
+	contentLength := CONTENT_LENGTH + strconv.Itoa(len(data)) + CRLF
+	response := OK + CRLF + contentType + contentLength + CRLF + data
+	fmt.Println(response)
+	conn.Write([]byte(response))
+}
+
+func HandleServerError(conn net.Conn) {
+	conn.Write([]byte(INTERNAL_SERVER_ERROR))
 }
 
 func HandleConn(conn net.Conn) {
@@ -38,14 +66,18 @@ func HandleConn(conn net.Conn) {
 	_, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading data")
-		os.Exit(1)
+		conn.Close()
+		return
 	}
 	req := bytes.Split(buffer, []byte(CRLF))
 	reqInfo := bytes.Split(req[0], []byte(WHITESPACE))
 	header := CreateHeader(string(reqInfo[0]), string(reqInfo[1]), string(reqInfo[2]))
-	if header.Path == FORWARD_SLASH {
-		conn.Write([]byte(OK))
-	} else {
+	switch {
+	case header.Path == FORWARD_SLASH:
+		conn.Write([]byte(OK + CRLF + CRLF))
+	case strings.HasPrefix(header.Path, ECHO):
+		HandleEchoRequest(conn, header)
+	default:
 		conn.Write([]byte(NOT_FOUND))
 	}
 }
